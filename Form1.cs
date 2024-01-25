5c01ef7e4c39edac53b8116569ebfae4c528c528
ABoba <a.boba@yandex.ru>
2024-01-25 12:11:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zxc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Меня зовут {textBox1.Text} " + $"Мне {textBox2.Text} лет " + $"Когда мне будет {Convert.ToInt32(textBox2.Text) + 10} я буду успешным программистом");
+        }
+    }
+}
